5a6a00e40d05f288ab754f2b3311f4477ac634bd
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:49:28 +0800

    fix SCRIPT_PATH value

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-SCRIPT_PATH=$(pwd)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ${SCRIPT_PATH}/init_submodule.sh dns_resolver git@github.com:ChenZaichuang/DNS-Resolver.git
 pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete
