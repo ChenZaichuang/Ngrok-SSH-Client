7380226ac3140a82f98a2a7b5181698d53e31468
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:30:13 +0800

    use pip3 instead of pip

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 SCRIPT_PATH=$(pwd)
 ${SCRIPT_PATH}/init_submodule.sh dns_resolver git@github.com:ChenZaichuang/DNS-Resolver.git
-pip install -r ${SCRIPT_PATH}/requirements.txt
+pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete
